bf4b5f5c49ad9bcede4a6001eefbad192978a746
Cheolhyeol <omarrapaz@gmail.com>
2024-10-22 19:27:02 -0300

    0.0.1b - Cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,js}"
-  ],
-  theme: {
-    extend: {},
+module.exports =
+{
+  content: ["./src/**/*.{html,js}"],
+  theme:
+  {
+    extend:
+    {
+      colors: 
+      {
+        azul:
+        {
+          5: '#323353',
+          4: '#484a77',
+          3: '#4d65b4',
+          2: '#4d9be6',
+          1: '#8fd3ff',
+        },
+      }
+    },
   },
   plugins: [],
 }
