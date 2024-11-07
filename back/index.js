a8136f7dc82d66264eaabbdf62a2545dae3143b5
Kayke Denelle <kaykedenelle@gmail.com>
2024-11-07 15:04:02 -0300

    1

--- back/index.js
+++ back/index.js
@@ -15,6 +15,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  return res.json("Funcionou");
+});
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/baralhos', deckRoutes);
