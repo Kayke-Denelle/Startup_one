8051972e4a725a9da2a213325036b79760a57bb8
Cheolhyeol <omarrapaz@gmail.com>
2024-12-03 15:39:54 -0300

    Removido o back

--- back/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { register, login } = require('../controllers/authControllers')
-const router = express.Router();
-
-router.post('/register', register);
-router.post('/login', login);
-
-module.exports = router;
