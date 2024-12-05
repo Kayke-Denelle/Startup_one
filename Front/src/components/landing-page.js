db4b0ffc7de214a437d85569877a8c71d763ee71
Cheolhyeol <omarrapaz@gmail.com>
2024-12-05 13:48:47 -0300

    a

--- Front/src/components/landing-page.js
+++ Front/src/components/landing-page.js
@@ -155,7 +155,8 @@ const LandingPage = () => {
                   </div>
                 </div>
               <div className="flex justify-center items-center mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-                <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+              </div>
+              <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
                   <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
                     <div className="flex flex-col-reverse gap-1">
                       <dt className="text-base text-cor-4/70">Usuários registrados</dt>
@@ -175,7 +176,6 @@ const LandingPage = () => {
                     </div>
                   </dl>
                 </div>
-              </div>
               </div>
             </section>
 
