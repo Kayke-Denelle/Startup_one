272f4ab4b9c48a407c8ecc73bb688629836deae7
Cheolhyeol <omarrapaz@gmail.com>
2024-12-05 13:51:09 -0300

    a

--- Front/src/components/landing-page.js
+++ Front/src/components/landing-page.js
@@ -154,8 +154,8 @@ const LandingPage = () => {
                     Imagem aquia
                   </div>
                 </div>
-              <div className="flex justify-center items-center mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
               </div>
+              <div className="flex justify-center items-center mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
               <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
                   <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
                     <div className="flex flex-col-reverse gap-1">
