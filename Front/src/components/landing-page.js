6919d2eafd7642422932e88397cc7fd2509b0ba5
Cheolhyeol <omarrapaz@gmail.com>
2024-12-05 12:53:05 -0300

    a

--- Front/src/components/landing-page.js
+++ Front/src/components/landing-page.js
@@ -144,7 +144,7 @@ const LandingPage = () => {
                   style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}>
                 </div>
               </div>
-              <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto max-w-7xl px-6 lg:px-8 flex justify-center items-center">
                 <div className="mx-auto max-w-2xl lg:mx-0 flex justify-center items-center">
                   <div>
                     <h2 class="text-cor-4 font-title text-4xl sm:text-5xl uppercase font-light text-balance max-w-6xl leading-snug text-g1 sm:leading-snug"><span class="text-cor-4 font-bold">Fortaleça </span>sua base de conhecimento</h2>
